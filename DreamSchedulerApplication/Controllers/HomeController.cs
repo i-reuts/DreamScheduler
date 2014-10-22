3399c058d1f25e073a360eae322b72e90b6b8b40
mv740 <michalwozniak@live.ca>
2014-10-21 21:20:34 -0400

    fix error - add if/else to protect the login

--- DreamSchedulerApplication/Controllers/HomeController.cs
+++ DreamSchedulerApplication/Controllers/HomeController.cs
@@ -70,38 +70,37 @@ namespace DreamSchedulerApp.Controllers
 
                 GraphClient client = new GraphClient(new Uri("http://localhost:7474/db/data"));
                 client.Connect();
-                //find user in database
+
+                var query = client
+                    .Cypher
+                    .Match("(n:Account)")
+                    .Where(((LoginTest n) => n.Username == username))
+                     .Return(n => n.As<LoginTest>())
+                        .Results;
+
                 try
                 {
-                    var query = client
-                    .Cypher
-                    .Match("(user:Account)")
-                    .Where((LoginTest account) => account.Password == password && account.Username == username)
-                    .Return(account => account.As<LoginTest>())
-                    .Results;
-                }
-                    //fuckkkkkkkkkkkkkkkkkkkkkk catch this shittttt if user not found catch error  NEED TO FIX this 
-                catch (Neo4jClient.NeoException error )
+                    var testing = query.Single();
+                    if (testing.Password == test.Password)
+                    {
+                        return RedirectToAction("index");
+                    }
+                    else
+                    {
+                        ViewBag.errorPassword = "password is wrong, try again";
+                        return View("login");
+                    }
+                }//if we can't find the account = it doest not exist, neo4j will cause a error 
+                catch (InvalidOperationException)
                 {
-                    if (error.NeoFullName == "org.neo4j.cypher.SyntaxException")
-                    {
-                        return View("login");
-                    }
-                    if( error.InnerException ==null)
-                    {
-                        return View("login");
-                    }
-                    if(error.HelpLink == null)
-                    {
-                        return View("login");
-                    }
+                    //error invalid user account 
+                    ViewBag.errorUsername = "this user does not exist ";
+                    return View("login");
                 }
-                
-                // save to db, for instance
-                return RedirectToAction("Contact");
             }
             // model is not valid
             return View("login", test);
+
         }
 
         public ActionResult Create()
@@ -117,13 +116,13 @@ namespace DreamSchedulerApp.Controllers
                 // save to db, for instance
                 GraphClient client = new GraphClient(new Uri("http://localhost:7474/db/data"));
                 client.Connect();
-                
+
                 //create newaccount object with input data
                 var newAccount = new LoginTest { Username = test.Username, Password = test.Password };
-               
+
                 // create the account in the database
                 client.Cypher
-                    .Create("(user:Account {newAcount})")
+                    .Create("(Account:Account {newAcount})")
                     .WithParam("newAcount", newAccount)//against cypher-injection 
                     .ExecuteWithoutResults();
                 return RedirectToAction("About");
